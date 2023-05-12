692b4239aabc40e5b1a2b15e1b644a890b285aee
Igor Fagundes <igorfgnds@gmail.com>
2023-05-11 22:36:28 -0300

    added unofficial site warning

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -2,10 +2,12 @@ import layout from '@/theme/layout.module.scss';
 
 import Header from '@/components/Layout/Header';
 import Footer from '@/components/Layout/Footer';
+import Unofficial from '@/components/Unofficial';
 
 const Layout = ({ children }: { children: React.ReactNode }): JSX.Element => {
   return (
     <>
+      <Unofficial />
       <Header />
       <main className={layout.main}>{children}</main>
       <Footer />
---
+++ src/components/Unofficial/index.tsx
@@ -0,0 +1,7 @@
+import styles from './styles.module.scss';
+
+const Unofficial = (): JSX.Element => {
+  return <div className={styles.unofficial}>Este site não é oficial</div>;
+};
+
+export default Unofficial;
